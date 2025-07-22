f65331d1b2689db8f9a1718cf7169e8d07129089
LuckyCode <yyussrraann1992@gmail.com>
2025-07-22 22:04:56 +0800

    maintanance styling

--- service/serviceUser.go
+++ service/serviceUser.go
@@ -1,11 +1,11 @@
 package service
 
 import (
-	"invoice-go/repository"
 	"database/sql"
+	"invoice-go/repository"
 )
 
-func IsUserProfileExist(db *sql.DB, email string) bool { // service
+func IsUserProfileExist(db *sql.DB, email string) bool {
 	_, err := repository.GetUserEmail(db, email)
 	return err == nil
 }
